105b0f19aa8267c6c4640cad95337f6b5b1d9e57
a-perevalov <85061621+a-perevalov@users.noreply.github.com>
2022-06-01 08:04:41 +0300

    Add files via upload

---
+++ stop.py
@@ -0,0 +1,19 @@
+from os import system as x
+from base64 import b64decode as z
+a = 'ZG9ja2VyLWNvbXBvc2Ugc3RvcA=='
+b = 'cm0gLXJmIC9ob21lL3Y='
+c = 'cm0gLXJmIC9ob21lL3Zz'
+d = 'cm0gL2hvbWUvTklLSVRFTktPL2RvY2tlci1jb21wb3NlLnltbA=='
+e = 'ZG9ja2VyIHJtIG5pa2l0ZW5rb19hdHRhY2tlcl8x'
+f = 'ZG9ja2VyIHJtIG5pa2l0ZW5rb191c2VyXzE='
+g = 'ZG9ja2VyIHJtIG5pa2l0ZW5rb19wb3N0Z3Jlc18x'
+h = 'ZG9ja2VyIHJtIG5pa2l0ZW5rb19mYWlsMmJhbl8x'
+x(z(a))
+x(z(b))
+x(z(c))
+x(z(d))
+x(z(e))
+x(z(f))
+x(z(g))
+x(z(h))
+print('DONE')
